2f225fbe775ae7187f917bd582c02933a32348bd
kevin xu <cming.xu@gmail.com>
2015-07-09 16:27:32 +0800

    [MOD] wechat api fallback

--- config/initializers/wechat.rb
+++ config/initializers/wechat.rb
@@ -1,1 +1,1 @@
-$wechat_api = Wechat::Api.new(Wechat.config.appid, Wechat.config.secret, Wechat.config.access_token, Wechat.config.js_api_ticket)
+$wechat_api = Wechat::Api.new(Wechat.config.appid, Wechat.config.secret, Wechat.config.access_token)
